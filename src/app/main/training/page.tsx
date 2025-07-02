a10a285718a82726bfe35c16cb521c1dda5144e9
lauvAri <198719993@qq.com>
2025-07-02 16:44:18 +0800

    中药制作页面添加视频资源

--- src/app/main/training/page.tsx
+++ src/app/main/training/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {useEffect, useState} from "react";
+import {Suspense, useEffect, useState} from "react";
 import { Input, Select, Card, List} from "antd";
 import Link from "next/link";
 import axiosInstance from "@/api/config";
@@ -58,86 +58,96 @@ export default function TrainingPage() {
     });
 
     return (
-        <div className="p-6">
-            {/* 搜索栏 */}
-            <div className="mb-6 max-w-md">
-                <Search
-                    placeholder="输入关键词搜索培训内容"
-                    onSearch={handleSearch}
-                    onChange={(e) => handleSearch(e.target.value)}
-                    value={searchText}
-                    size="large"
-                />
-            </div>
+        <>
+            <Suspense fallback={<div>加载中...</div>}>
+                <video src={'/中药制作.mp4'} autoPlay muted loop onError={() => <></>}
+                       width={"100%"} style={{
+                    margin: "0 auto",
+                    marginBottom: "1rem",
+                }}
+                ></video>
+            </Suspense>
 
-            <div className="flex">
-                {/* 左侧筛选组件 */}
-                <div className="w-64 mr-6">
-                    <Card title="按中药材分类" className="h-fit">
-                        <Select
-                            defaultValue="all"
-                            onChange={handleCategoryChange}
-                            className="w-full"
-                            size="large"
-                        >
-                            {categories.map((category, index) => (
-                                <Option key={index} value={category}>
-                                    {category}
-                                </Option>
-                            ))}
-                        </Select>
-                    </Card>
+            <div className="p-6">
+                {/* 搜索栏 */}
+                <div className="mb-6 max-w-md">
+                    <Search
+                        placeholder="输入关键词搜索培训内容"
+                        onSearch={handleSearch}
+                        onChange={(e) => handleSearch(e.target.value)}
+                        value={searchText}
+                        size="large"
+                    />
                 </div>
 
-                {/* 右侧筛选结果列表 */}
-                <div className="flex-1">
-                    <List
-                        grid={{ gutter: 16, column: 3 }}
-                        dataSource={filteredData}
-                        renderItem={(item) => (
-                            <List.Item>
-                                <Card
-                                    style = {{
-                                        backgroundImage: "linear-gradient(to right bottom , oklch(75% 0.183 55.934), oklch(85.2% 0.199 91.936) )"
-                                    }}
-                                    hoverable
-                                    className="h-full"
-                                    title={"培训材料: "+item.title}
-                                    extra={(<div className={"flex gap-4"}>
-                                        <div>观看次数: {item.count}</div>
-                                        <Link href={`/main/training/detail?key=${item.id}`}>
+                <div className="flex">
+                    {/* 左侧筛选组件 */}
+                    <div className="w-64 mr-6">
+                        <Card title="按中药材分类" className="h-fit">
+                            <Select
+                                defaultValue="all"
+                                onChange={handleCategoryChange}
+                                className="w-full"
+                                size="large"
+                            >
+                                {categories.map((category, index) => (
+                                    <Option key={index} value={category}>
+                                        {category}
+                                    </Option>
+                                ))}
+                            </Select>
+                        </Card>
+                    </div>
+
+                    {/* 右侧筛选结果列表 */}
+                    <div className="flex-1">
+                        <List
+                            grid={{ gutter: 16, column: 3 }}
+                            dataSource={filteredData}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    <Card
+                                        style = {{
+                                            backgroundImage: "linear-gradient(to right bottom , oklch(75% 0.183 55.934), oklch(85.2% 0.199 91.936) )"
+                                        }}
+                                        hoverable
+                                        className="h-full"
+                                        title={"培训材料: "+item.title}
+                                        extra={(<div className={"flex gap-4"}>
+                                            <div>观看次数: {item.count}</div>
+                                            <Link href={`/main/training/detail?key=${item.id}`}>
                                             <span style={{
                                                 color: "#fff",
                                                 textDecoration: "underline",
                                                 fontWeight: "bold"
                                             }}>编号: {item.id}</span>
-                                        </Link>
-                                    </div>)}>
-                                    <div className={'flex gap-2 my-2'}>
-                                        <div className={"flex gap-2  border-2  rounded-md px-2 py-1 items-center m-tag"}>
-                                            <GiHerbsBundle /><span>{item.herbName}</span>
+                                            </Link>
+                                        </div>)}>
+                                        <div className={'flex gap-2 my-2'}>
+                                            <div className={"flex gap-2  border-2  rounded-md px-2 py-1 items-center m-tag"}>
+                                                <GiHerbsBundle /><span>{item.herbName}</span>
+                                            </div>
+                                            <div className={"flex gap-2  border-2 rounded-md px-2 py-1 items-center m-tag"}>
+                                                <BiCategoryAlt /><span>{item.type}</span>
+                                            </div>
+                                            <div className={"flex gap-2  border-2  rounded-md px-2 py-1 items-center m-tag"}>
+                                                <TbUserEdit /><span>{item.username}</span>
+                                            </div>
+                                            <div className={"flex gap-2  border-2  rounded-md px-2 py-1 items-center m-tag"}>
+                                                <CiTimer /><span>{new Date(item.time).toLocaleDateString()}</span>
+                                            </div>
                                         </div>
-                                        <div className={"flex gap-2  border-2 rounded-md px-2 py-1 items-center m-tag"}>
-                                            <BiCategoryAlt /><span>{item.type}</span>
-                                        </div>
-                                        <div className={"flex gap-2  border-2  rounded-md px-2 py-1 items-center m-tag"}>
-                                            <TbUserEdit /><span>{item.username}</span>
-                                        </div>
-                                        <div className={"flex gap-2  border-2  rounded-md px-2 py-1 items-center m-tag"}>
-                                            <CiTimer /><span>{new Date(item.time).toLocaleString()}</span>
-                                        </div>
-                                    </div>
-                                    <Card.Meta description={item.des} />
-                                </Card>
-                            </List.Item>
-                        )}
-                        locale={{
-                            emptyText: '没有找到匹配的培训内容'
-                        }}
-                    />
-                </div>
+                                        <Card.Meta description={item.des} />
+                                    </Card>
+                                </List.Item>
+                            )}
+                            locale={{
+                                emptyText: '没有找到匹配的培训内容'
+                            }}
+                        />
+                    </div>
 
-                <style jsx>{`
+                    <style jsx>{`
                     .m-tag {
                         color: #000;
                         background-color: #fff;
@@ -148,7 +158,9 @@ export default function TrainingPage() {
                         background-color: #000;
                     }
                 `}</style>
+                </div>
             </div>
-        </div>
+        </>
+
     );
 }
